260199ec5b881b034f4b2a342650bd1bebfc7c5d
Kar65 <Karen65@njit.edu>
2025-11-24 12:50:16 -0500

    Saving payload files before switching branches

---
+++ Project/Common/PointsPayload.java
@@ -0,0 +1,24 @@
+package Project.Common;
+
+public class PointsPayload extends Payload {
+
+    private int points;
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "PointsPayload[%s] Client Id [%s] Points: [%d]",
+                getPayloadType(),
+                getClientId(),
+                points
+        );
+    }
+}
